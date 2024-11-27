406970b2e49514466bd1e81bea2bc9a4924038b8
Gurban Khaniyev <qxaniyev@gmail.com>
2024-11-27 22:10:17 +0400

    added admin login

---
+++ My_first_code/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        string a = Console.ReadLine();
+        string b = Console.ReadLine();
+        bool isAdmin = Program.Login(a, b);
+        if (isAdmin)
+        {
+            Console.WriteLine("Ugurlu");
+        }
+        else
+        {
+            Console.WriteLine("ugursuz");
+        }
+    }
+    static bool Login(string name, string password)
+    {
+        if (name == "qurban" && password == "1234")
+        {
+            return true;
+        }
+        return false;
+    }
+}
